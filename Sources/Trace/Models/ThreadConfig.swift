3539ad01f5062f82268519b224f55bb6ca723611
SamSong1997 <779295195@qq.com>
2026-04-09 10:05:32 +0800

    feat: add ThreadConfig data model

---
+++ Sources/Trace/Models/ThreadConfig.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct ThreadConfig: Identifiable, Codable, Equatable {
+    let id: UUID
+    var name: String
+    var targetFile: String
+    var icon: String?
+    var order: Int
+
+    init(
+        id: UUID = UUID(),
+        name: String,
+        targetFile: String,
+        icon: String? = nil,
+        order: Int = 0
+    ) {
+        self.id = id
+        self.name = name
+        self.targetFile = targetFile
+        self.icon = icon
+        self.order = order
+    }
+
+    static func == (lhs: ThreadConfig, rhs: ThreadConfig) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    static let `default` = ThreadConfig(
+        name: "想法",
+        targetFile: "Threads/想法.md",
+        icon: "lightbulb"
+    )
+}
